41d7534d869a86a3c883495af07f7ee556bcfd34
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2021-09-26 19:23:04 +0300

    Added home and cube controllers

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const initHandlebars = require('./config/handlebars');
 
+const routes = require('./routes');
+
 const app = express();
 
 initHandlebars(app);
 
 app.use(express.static('static'));
 
-app.all('/', (req, res) => {
-    res.render('index');
-});
+app.use(routes);
 
 app.listen(5000, console.log.bind(console, 'App is listening on port 5000'));
