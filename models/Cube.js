9c1386bcbedd8aee001537d264fc60d6457267c8
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2021-09-27 21:44:44 +0300

    Cube partial for the home page added

--- models/Cube.js
+++ models/Cube.js
@@ -1,24 +1,35 @@
+const uniqid = require('uniqid');
+
 class Cube {
-    static cubes = [{
+    static #cubes = [{
+        id: 'jhdfkgeriuew',
         name: 'Rubik',
         description: '3x3 rubik standart',
         imageURL: 'https://static.scientificamerican.com/sciam/cache/file/76CA6AF7-D83E-481F-B27BED55698AEB36_source.jpg',
         difficulty: '3'
+    },
+    {
+        id: 'ddqj63bj8ku2zvejd',
+        name: 'Mirror cube',
+        description: 'mirror cube',
+        imageURL: 'https://rukminim1.flixcart.com/image/416/416/k20r8nk0/puzzle/r/4/p/1-3x3-silver-mirror-cube-shengshou-original-imafhgmzdh8dfetk.jpeg?q=70',
+        difficulty: '4'
     }];
 
     constructor(name, description, imageURL, difficulty) {
+        this.id = uniqid();
         this.name = name;
         this.description = description;
         this.imageURL = imageURL;
         this.difficulty = difficulty;
     }
 
-    static getAll() {
-        return Cube.cubes.slice();
+    static get cubes() {
+        return Cube.#cubes.slice();
     }
 
     static add(cube) {
-        Cube.cubes.push(cube);
+        Cube.#cubes.push(cube);
     }
 }
 
