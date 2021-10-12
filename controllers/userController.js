17c7dee66cf071a6ad979b255419b9aa1cb5d0e9
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2021-10-12 16:27:07 +0300

    add login post action

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,12 @@ router.get('/login', (req, res) => {
     res.render('user/login');
 });
 
+router.post('/login', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('login');
+});
+
 router.get('/register', (req, res) => {
     res.render('user/register');
 });
